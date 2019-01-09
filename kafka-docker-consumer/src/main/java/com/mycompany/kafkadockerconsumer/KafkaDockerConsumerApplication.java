ba3dcb1032f007f4242051187f881e6d335d11b3
Wagner Franchin <wagner.franchin@oracle.com>
2019-01-09 19:07:48 -0200

    Fixed some code indentation

--- kafka-docker-consumer/src/main/java/com/mycompany/kafkadockerconsumer/KafkaDockerConsumerApplication.java
+++ kafka-docker-consumer/src/main/java/com/mycompany/kafkadockerconsumer/KafkaDockerConsumerApplication.java
@@ -13,53 +13,53 @@ import java.util.*;
 @SpringBootApplication
 public class KafkaDockerConsumerApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaDockerConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDockerConsumerApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) {
+    @Override
+    public void run(String... args) {
 
-		String bootstrap_server;
-		List<String> topics;
-		String group_id;
+        String bootstrap_server;
+        List<String> topics;
+        String group_id;
 
-		if (args.length < 3) {
-			System.err.println("java -jar target/kafka-docker-consumer <bootstrap_server> <group_id> <topics>");
-			System.err.println("Example: java -jar target/kafka-docker-consumer localhost:29092 my-group BTC,LTC");
-			System.exit(1);
-		}
+        if (args.length < 3) {
+            System.err.println("java -jar target/kafka-docker-consumer <bootstrap_server> <group_id> <topics>");
+            System.err.println("Example: java -jar target/kafka-docker-consumer localhost:29092 my-group BTC,LTC");
+            System.exit(1);
+        }
 
-		bootstrap_server = args[0];
-		group_id = args[1];
-		topics = Arrays.asList(args[2].split(","));
+        bootstrap_server = args[0];
+        group_id = args[1];
+        topics = Arrays.asList(args[2].split(","));
 
-		Properties properties = new Properties();
-		properties.put("bootstrap.servers", bootstrap_server);
-		properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		properties.put("group.id", group_id);
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", bootstrap_server);
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("group.id", group_id);
 
-		KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
-		kafkaConsumer.subscribe(topics);
-		try{
-			while (true){
-				ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
-				for (ConsumerRecord record: records){
-					System.out.println(String.format("[%s] Topic - %s, Partition - %d, Value: %s", LocalTime.now().toString(), record.topic(),
-						record.partition(), getRecordCryptoCurrentValue(record)));
-				}
-			}
-		}catch (Exception e){
-			System.out.println(e.getMessage());
-		}finally {
-			kafkaConsumer.close();
-		}
-	}
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(topics);
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
+                for (ConsumerRecord record : records) {
+                    System.out.println(String.format("[%s] Topic - %s, Partition - %d, Value: %s", LocalTime.now().toString(), record.topic(),
+                        record.partition(), getRecordCryptoCurrentValue(record)));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            kafkaConsumer.close();
+        }
+    }
 
-	private String getRecordCryptoCurrentValue(ConsumerRecord record) {
-		String info = (String) record.value();
-		String value = info.substring(info.indexOf("\"last\""), info.indexOf(", \"times")).replace("\"", "").trim();
-		return value.substring(value.indexOf(":") + 1).trim();
-	}
+    private String getRecordCryptoCurrentValue(ConsumerRecord record) {
+        String info = (String) record.value();
+        String value = info.substring(info.indexOf("\"last\""), info.indexOf(", \"times")).replace("\"", "").trim();
+        return value.substring(value.indexOf(":") + 1).trim();
+    }
 }
--- kafka-docker-producer/src/main/java/com/mycompany/kafkadockerproducer/KafkaDockerProducerApplication.java
+++ kafka-docker-producer/src/main/java/com/mycompany/kafkadockerproducer/KafkaDockerProducerApplication.java
@@ -19,91 +19,91 @@ import java.util.Properties;
 @SpringBootApplication
 public class KafkaDockerProducerApplication implements CommandLineRunner {
 
-	private KafkaProducer<String,String> kafkaProducer;
-	private Long interval;
+    private KafkaProducer<String, String> kafkaProducer;
+    private Long interval;
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaDockerProducerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDockerProducerApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		String bootstrap_server;
-		Long initial_delay;
-		List<CryptoType> listTopics;
+    @Override
+    public void run(String... args) throws Exception {
+        String bootstrap_server;
+        Long initial_delay;
+        List<CryptoType> listTopics;
 
-		if (args.length < 4) {
-			System.err.println("java -jar target/kafka-docker-producer <bootstrap-server> <initial_delay> <interval> <topic-1> -- <topic-n>");
-			System.err.println("Example: java -jar target/kafka-docker-producer localhost:29092 1000 3000 BTC,LTC,ETH");
-			System.exit(1);
-		}
+        if (args.length < 4) {
+            System.err.println("java -jar target/kafka-docker-producer <bootstrap-server> <initial_delay> <interval> <topic-1> -- <topic-n>");
+            System.err.println("Example: java -jar target/kafka-docker-producer localhost:29092 1000 3000 BTC,LTC,ETH");
+            System.exit(1);
+        }
 
-		listTopics = new ArrayList<>();
-		bootstrap_server = args[0];
-		initial_delay = Long.valueOf(args[1]);
-		interval = Long.valueOf(args[2]);
-		String[] topics = args[3].split(",");
-		for (String t: topics) {
-			listTopics.add(FactortyCryptoType.getCryptoType(t));
-		}
+        listTopics = new ArrayList<>();
+        bootstrap_server = args[0];
+        initial_delay = Long.valueOf(args[1]);
+        interval = Long.valueOf(args[2]);
+        String[] topics = args[3].split(",");
+        for (String t : topics) {
+            listTopics.add(FactortyCryptoType.getCryptoType(t));
+        }
 
-		Thread.sleep(initial_delay);
+        Thread.sleep(initial_delay);
 
-		Properties properties = new Properties();
-		properties.put("bootstrap.servers", bootstrap_server);
-		properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		properties.put("acks", "1");
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", bootstrap_server);
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("acks", "1");
 
-		kafkaProducer = new KafkaProducer<>(properties);
+        kafkaProducer = new KafkaProducer<>(properties);
 
-		String data;
-		try {
-			while(true){
-				for (CryptoType crypto: listTopics) {
-					if (crypto != CryptoType.UNKNOW) {
-						sendData(crypto.getInitials(), crypto.getUrl());
-					}
-				}
-			}
-		}
-		finally {
-			kafkaProducer.close();
-		}
-	}
+        String data;
+        try {
+            while (true) {
+                for (CryptoType crypto : listTopics) {
+                    if (crypto != CryptoType.UNKNOW) {
+                        sendData(crypto.getInitials(), crypto.getUrl());
+                    }
+                }
+            }
+        } finally {
+            kafkaProducer.close();
+        }
+    }
 
-	private void sendData(String topic, String url) throws InterruptedException, IOException {
-		String data = getRequest(url);
-		ProducerRecord<String, String> record = new ProducerRecord<>(topic, topic, data);
-		kafkaProducer.send(record, (recordMetadata, e) -> {
-				if (e != null)
-					e.printStackTrace();
-				else
-					System.out.println(String.format("[%s] JSON data sent to topic %s.", LocalTime.now().toString(), topic));
-			}
-		);
-		Thread.sleep(interval);
-	}
+    private void sendData(String topic, String url) throws InterruptedException, IOException {
+        String data = getRequest(url);
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, topic, data);
+        kafkaProducer.send(record, (recordMetadata, e) -> {
+            if (e != null) {
+                e.printStackTrace();
+            } else {
+                System.out.println(String.format("[%s] JSON data sent to topic %s.", LocalTime.now().toString(), topic));
+            }
+            }
+        );
+        Thread.sleep(interval);
+    }
 
-	private String getRequest(String url) throws IOException {
-		StringBuilder response = new StringBuilder();
+    private String getRequest(String url) throws IOException {
+        StringBuilder response = new StringBuilder();
 
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-		con.setRequestMethod("GET");
-		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
-		String inputLine;
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
 
-		in.close();
+        in.close();
 
-		return response.toString();
-	}
+        return response.toString();
+    }
 }
 
